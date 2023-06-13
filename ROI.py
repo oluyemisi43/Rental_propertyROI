b6f84f50380335d9d27c28cedf10b04e289c3c7d
oluyemisi olusola <77764812+oluyemisi43@users.noreply.github.com>
2023-06-13 15:58:39 -0500

    revised ROI

--- ROI.py
+++ ROI.py
@@ -1,48 +1,21 @@
-class Income:
-    def __init__(self, rental, laundry, storage, misc):
-        self.rental = rental
-        self.laundry = laundry
-        self.storage = storage
-        self.misc = misc
+
         
-    def calculate_total_income(self):
-        return self.rental+self.laundry+self.storage+self.misc
-class Expense:
-    def __init__(self, taxes, insurance, util, hoa, vacancy, repairs, capex, mortgage, propertymgt):
-        self.taxes = taxes
-        self.insurance = insurance
-        self.util = util
-        self.hoa = hoa
-        
-        self.vacancy = vacancy
-        self.repairs = repairs
-        self.capex = capex
-        self.mortgage = mortgage
-        self.propertymgt = propertymgt
-        
-    def calculate_total_expense(self):
-        return self.taxes+self.insurance+self.util+self.hoa+self.vacancy+self.repairs+self.capex+self.mortgage+self.propertymgt
-class Cash_flow:
-    def __init__(self,income,expense):
-        self.income=income
-        self.expense=expense
+class ROI:
+    def __init__(self):
+        self.income=0
+        self.expenses=0
+        self.cashflow=0
+        self.investmentscost=0
+    def calculate_total_income(self,rental,laundry,storage,misc):
+        self.income=(rental+laundry+storage+misc)
+    def calculate_total_expense(self,taxes,insurance,utilites,hoa,vacancy,repairs,capex,mortgage,propertymgt):
+        self.expenses= taxes+insurance+utilites+hoa+vacancy+repairs+capex+mortgage+propertymgt
     def calculate_cash_flow(self): 
-        return self.income-self.expense
-class Cash_on_cash_ROI:
-    def __init__(self, monthly_cash_flow, down_payment, closing_cost, rehab_budget, misc_other):
-        self.monthly_cash_flow = monthly_cash_flow
-        self.down_payment = down_payment
-        self.closing_cost = closing_cost
-        self.rehab_budget = rehab_budget
-        self.misc_other = misc_other
-        
-    def calculate_total_investment(self):
-        return self.down_payment+self.closing_cost+self.rehab_budget+self.misc_other
-    def annual_cash_flow(self):
-        return self.monthly_cash_flow*12
+        self.cashflow= self.income-self.expenses
+    def calculate_total_investment(self,down_payment,closing_cost,rehab_budget,misc_other):
+        self.investmentscost= down_payment+closing_cost+rehab_budget+misc_other
     def calculate_ROI(self):
-        a=self.annual_cash_flow()
-        b=self.calculate_total_investment()
-        return a/b
-        
-        
+        a=self.cashflow*12
+        b=self.investmentscost
+        return a/b    
+            
--- ROI_calculate.py
+++ ROI_calculate.py
@@ -1,12 +1,12 @@
-from ROI import Income,Expense,Cash_flow,Cash_on_cash_ROI
-
+from ROI import ROI
+roi=ROI()
 print("welcome to the ROI calculator")
 print ("please answer questions regarding monthly income")
 rental = input("Enter your monthly rental income: ")
 laundry = input("Enter your monthly laundry income: ")
 storage = input("Enter your monthly storage income: ")
 misc = input("Enter your monthly misc income: ")
-i=Income(int(rental),int(laundry),int(storage),int(misc)).calculate_total_income()
+roi.calculate_total_income(int(rental),int(laundry),int(storage),int(misc))
 
 print ("please answer questions regarding monthly expenses")
 taxes = input("Enter your monthly tax expense: ")
@@ -18,15 +18,16 @@ repairs = input("Enter your monthly repairs expense: ")
 capex = input("Enter your monthly capital expense: ")
 mortgage = input("Enter your monthly mortgage expense: ")
 propertymgt = input("Enter your monthly property management expense: ")
-e=Expense(int(taxes),int(insurance),int(util),int(hoa),int(vacancy),int(repairs),int(capex),int(mortgage),int(propertymgt)).calculate_total_expense()
+roi.calculate_total_expense(int(taxes),int(insurance),int(util),int(hoa),int(vacancy),int(repairs),int(capex),int(mortgage),int(propertymgt))
 
-c=Cash_flow(int(i),int(e)).calculate_cash_flow()
-
+roi.calculate_cash_flow()
+    
 print ("please answer questions regarding investments cost")
 down = input("Enter your down payment: ")
 closing = input("Enter your closing costs: ")
 rehab = input("Enter your rehab budget: ")
 misc_other = input("Enter your any other misc costs: ")
-r=Cash_on_cash_ROI(int(c),int(down),int(closing),int(rehab),int(misc_other)).calculate_ROI()
+roi.calculate_total_investment(int(down),int(closing),int(rehab),int(misc_other))
+r=roi.calculate_ROI()
 print("your cash on cash ROI is")
 print (r*100)
