3c5a9093465292d67223db0ae6dfb955ce1deb68
oluyemisi olusola <77764812+oluyemisi43@users.noreply.github.com>
2023-06-28 14:11:39 -0500

    revised ROI

--- ROI.py
+++ ROI.py
@@ -17,5 +17,38 @@ class ROI:
     def calculate_ROI(self):
         a=self.cashflow*12
         b=self.investmentscost
-        return a/b    
+        return a/b
+        
+roi=ROI()
+print("welcome to the ROI calculator")
+print ("please answer questions regarding monthly income")
+rental = input("Enter your monthly rental income: ")
+laundry = input("Enter your monthly laundry income: ")
+storage = input("Enter your monthly storage income: ")
+misc = input("Enter your monthly misc income: ")
+roi.calculate_total_income(int(rental),int(laundry),int(storage),int(misc))
+
+print ("please answer questions regarding monthly expenses")
+taxes = input("Enter your monthly tax expense: ")
+insurance = input("Enter your monthly insurance expense: ")
+util = input("Enter your monthly utilities expense (including electric, water, sewer, trash, gas, and landscape): ")
+hoa = input("Enter your monthly HOA expense: ")
+vacancy = input("Enter your monthly vacancy expense: ")
+repairs = input("Enter your monthly repairs expense: ")
+capex = input("Enter your monthly capital expense: ")
+mortgage = input("Enter your monthly mortgage expense: ")
+propertymgt = input("Enter your monthly property management expense: ")
+roi.calculate_total_expense(int(taxes),int(insurance),int(util),int(hoa),int(vacancy),int(repairs),int(capex),int(mortgage),int(propertymgt))
+
+roi.calculate_cash_flow()
+    
+print ("please answer questions regarding investments cost")
+down = input("Enter your down payment: ")
+closing = input("Enter your closing costs: ")
+rehab = input("Enter your rehab budget: ")
+misc_other = input("Enter your any other misc costs: ")
+roi.calculate_total_investment(int(down),int(closing),int(rehab),int(misc_other))
+r=roi.calculate_ROI()
+print("your cash on cash ROI is")
+print (r*100)     
             
